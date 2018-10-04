5130487294753787ac160389634297d22a1c9a92
Yusupov Danil <danil.usupov@icl-services.com>
2018-10-04 16:46:39 +0300

    Added reducers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
 import './App.css'
+import { connect } from 'react-redux'
 
 class App extends Component {
   render() {
+    const { name, surname, age } = this.props.user
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Top of my photos</h1>
+          <h1 className="App-title">Мой топ фото</h1>
         </header>
-        <p className="App-intro">Here is my most 'liked' photos</p>
+        <p className="App-intro">Здесь будут мои самые "залайканные" фотки</p>
+        <p>
+          Привет из App, {name} {surname}!
+        </p>
+        <p>Тебе уже {age}?</p>
       </div>
     )
   }
 }
 
-export default App
+// приклеиваем данные из store
+const mapStateToProps = store => {
+  console.log(store) // посмотрим, что же у нас в store?
+  return {
+    user: store.user,
+  }
+}
+
+// в наш компонент App, с помощью connect(mapStateToProps)
+export default connect(mapStateToProps)(App)
---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+export const initialState = {
+  user: {
+    name: 'Данил',
+    surname: 'Юсупов',
+    age: 25,
+  },
+}
+
+export const rootReducer = (state = initialState) => {
+  return state
+}
